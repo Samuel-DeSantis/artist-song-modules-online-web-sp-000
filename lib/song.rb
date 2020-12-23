16981b354274405eba383d5b0b81303cb019fff6
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-12-23 05:00:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,14 +6,14 @@ class Song
   attr_accessor :name
   attr_reader :artist
 
-  @@songs = []
+  @@ = []
 
   def initialize
     @@songs << self
   end
 
   def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
+    self.all.detect{|a| a.name == name}
   end
 
   def self.all
